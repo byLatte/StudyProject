9469fa8e609a5f94f4f10179bbe2624809098b91
dev21 <dev21>
2021-08-08 15:05:19 +0900

    init

---
+++ src/main/java/com/spring/project/index/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.spring.project.index.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/index")
+    public String index(Model model){
+        return "view/index";
+    }
+}
